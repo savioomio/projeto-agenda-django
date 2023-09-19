7bd08ec379f717a7090944c99fb2ab13781c2785
savioomio <saviopessoaafonso@gmail.com>
2023-09-18 21:41:54 -0300

    'teste_templates'

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+
 def home(request):
+    context = {
+        'teste': 'tetsejjfjfsdadadasjfjfj',
+    }
 
     return render(
         request,
         'home.html',
+        context
     )
     
 
