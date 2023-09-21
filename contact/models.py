40e9866da7b015d481b69b20c16c27d3f3f77ca8
savioomio <saviopessoaafonso@gmail.com>
2023-09-21 11:51:37 -0300

    desenvouvendo os models.py

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -8,6 +19,20 @@ class contact(models.Model):
     emial = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/') 
+    category = models.ForeignKey(
+        category, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
