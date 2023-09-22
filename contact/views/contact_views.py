2370968414b38c6bf2da4740a801ce1be5ffeded
savioomio <saviopessoaafonso@gmail.com>
2023-09-22 06:03:17 -0300

    organizando as views do meu projeto

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {
-        'teste': 'tetsejjfjfsdadadasjfjfj',
-    }
-
     return render(
         request,
         'contact/index.html',
-        context
     )
-    
-
-
-
--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,8 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
