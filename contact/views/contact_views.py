349252e8ec9948648d2e0a2f12199026991444e4
savioomio <saviopessoaafonso@gmail.com>
2023-09-27 06:06:16 -0300

    exibindo na tela os contatos do banco de dados

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from contact.models import contact
 
 def index(request):
+    contacts = contact.objects.filter(show=True).order_by('-id')[:20]
+
+    context = {
+        'contacts': contacts,
+    }
     return render(
         request,
         'contact/index.html',
+        context,
     )
