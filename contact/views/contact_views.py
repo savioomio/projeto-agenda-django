c8adbb11311c3128c22e9793b600db83af8df3fb
savioomio <saviopessoaafonso@gmail.com>
2023-10-07 08:21:20 -0300

    implementandoa funcionalidade de pesquisa de contatos do usuario

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -10,6 +10,35 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'search',
+        'search_value': search_value,
     }
 
     return render(
@@ -25,8 +54,11 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name}'
+
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
