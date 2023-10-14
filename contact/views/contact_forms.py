f3082a620b79bc740ca16871a90b7f12052cb2ea
savioomio <saviopessoaafonso@gmail.com>
2023-10-14 06:54:50 -0300

    fazendo a views do meu form de cadastro de contatos

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,28 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',  
+        )
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST), 
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
-        
+        'form': ContactForm(), 
     }
-
     return render(
         request,
         'contact/create.html',
