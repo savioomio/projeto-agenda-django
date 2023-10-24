18a6b15a2d3d74895833179f7ef6de71ae8b491b
savioomio <saviopessoaafonso@gmail.com>
2023-10-23 22:29:57 -0300

    brincando cokm bootstrap

--- project/settings.py
+++ project/settings.py
@@ -104,7 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+#(para o sistema em produção)
+ALLOWED_HOSTS = ['sistema-treinos-de-savio.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [
+    "https://sistema-treinos-de-savio.up.railway.app",
+]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
