893429f418f3d29cc18feb0666a60861753cc618
savioomio <saviopessoaafonso@gmail.com>
2023-10-31 05:50:56 -0300

    adicionado a funcionalidad de de deletar o usuario

--- contact/urls.py
+++ contact/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # contacts (CRUD)
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
-    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
 
     path('search/', views.search, name='search'),
